7ca234b95ec268468e06b285485a54f4f8fb8f21
Jim MacArthur <jim@mode7.co.uk>
2018-06-09 15:38:23 +0100

    Row selectors part 1

--- main.py
+++ main.py
@@ -10,9 +10,27 @@ def makeBox(x, y, width, height):
 # Circle example
 #shape=circleShape(radius=6.35,pos=(0,10)),
 
+selector_rods = 3
+memory_rows = 1<<selector_rods
+
 class Memory (Framework):
     name = ""
 
+
+    def slide_joint(self, body1, body2, axis, limit1, limit2):
+        return self.world.CreatePrismaticJoint(
+            bodyA=body1, 
+            bodyB=body2,
+            anchor=body2.worldCenter,
+            axis=axis,
+            maxMotorForce=10000.0,
+            enableMotor=True,
+            lowerTranslation=limit1,
+            upperTranslation=limit2,
+            enableLimit=True,
+            )
+        
+    
     def __init__(self):
         super(Memory, self).__init__()
 
@@ -43,17 +61,9 @@ class Memory (Framework):
         
         injectors=[]
         for col in range(0,8):
-            injectors.append(self.world.CreateDynamicBody(position=(0, 7+14*col), fixtures=row_injector_fixtures))
-            self.world.CreatePrismaticJoint(
-                bodyA=injectors[-1], 
-                bodyB=groundBody,
-                anchor=groundBody.worldCenter,
-                axis=(1, 0),
-                lowerTranslation=7,
-                upperTranslation=17,
-                enableLimit=True
-            )
-                            
+            injector = self.world.CreateDynamicBody(position=(0, 7+14*col), fixtures=row_injector_fixtures)
+            injectors.append(injector)
+            self.slide_joint(injector, groundBody, (1,0), 7, 17)
                              
         row_ejector_fixtures = []
         for col in range(0,8):
@@ -65,37 +75,35 @@ class Memory (Framework):
                 position=(0, 14*col),
                 fixtures=row_ejector_fixtures,
             )
-            pj2 = self.world.CreatePrismaticJoint(
-                bodyA=ejector, 
-                bodyB=groundBody,
-                anchor=groundBody.worldCenter,
-                axis=(1, 0),
-                maxMotorForce=100.0,
-                motorSpeed=0.0,
-                enableMotor=True,
-                lowerTranslation=0,
-                upperTranslation=14,
-                enableLimit=True,
-
-            )
+            ejectors.append(ejector)
+            self.slide_joint(ejector, groundBody, (1,0), 0, 14)
 
 
-        row_selector_fixtures = []
-        for row in range(0,8):
-            selector_no = 0
-            enabled = (row >> selector_no) & 1
-            row_selector_fixtures.append(
-                fixtureDef(shape=makeBox(14,14*row+7*enabled,14,7),             density=1.0,
-                           filter=filter(groupIndex=1, categoryBits=0x0001, maskBits=0xFFFF))
+        for selector_no in range(0,selector_rods):
+            row_selector_fixtures = []
+            for row in range(0,8):
+                enabled = (row >> selector_no) & 1
+                row_selector_fixtures.append(
+                    fixtureDef(shape=makeBox(14+25*selector_no,14*row+7*enabled,14,7),             density=1.0,
+                               filter=filter(groupIndex=1, categoryBits=0x0001, maskBits=0xFFFF))
                 )
             
-        row_selector = self.world.CreateDynamicBody(
-            position=(200, 0),
-            fixtures=row_selector_fixtures,
-        )
+            row_selector = self.world.CreateDynamicBody(
+                position=(200, 0),
+                fixtures=row_selector_fixtures,
+            )
+            self.slide_joint(row_selector, groundBody, (0,1), -7, 0)
 
-
+        for row in range(0,8):
+            row_follower_fixtures = []
+            for selector_no in range(0,selector_rods):
+                row_follower_fixtures.append(fixtureDef(shape=circleShape(radius=6.35/2, pos=(selector_no*25+35,14*row+10)), density=1.0, filter=filter(groupIndex=1, categoryBits=0x0002, maskBits=0xFFFE)))
+            ejector = self.world.CreateDynamicBody(
+                position=(200, 0),
+                fixtures=row_follower_fixtures)
             
+            self.slide_joint(ejector, groundBody, (1,0), limit1=0, limit2=20)
+    
     def Step(self, settings):
         super(Memory, self).Step(settings)
 
