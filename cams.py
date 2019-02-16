25d7ade63b0575456c9fd2fd9462d6ffe9ab5e16
Jim MacArthur <jim@mode7.co.uk>
2019-02-16 16:28:41 +0000

    Rename some control lines

--- cams.py
+++ cams.py
@@ -12,8 +12,8 @@ class Cam(_Cam):
 instruction_ready_point = 0.50 # Instruction decoder should be set up, ready for cams to use
 
 cams = [
-    Cam(300, 200, [(0.0,0.02)], 1, "PC INJECTOR", horizontal=False),
-    Cam(150,300, [(0.05,0.07), (0.32, 0.06)], 0, "MEMORY DECODER INPUT HOLDOFF", horizontal=False),
+    Cam(300, 200, [(0.0,0.02)], 1, "PC INJECTOR"),
+    Cam(150,300, [(0.05,0.07), (0.32, 0.06)], 0, "MEMORY DECODER INPUT HOLDOFF"),
     Cam(-400, 120, [(0.02, 0.07), (0.31,0.1), (0.64,0.1), (0.95,0.03)], -1, "MEMORY RETURN", horizontal=True),
     Cam(-300,100, [(0.03,0.11), (0.17,0.05), (0.31,0.1), (0.48,0.05), (0.65,0.1), (0.96,0.03)], -1, "MEMORY DECODER OUTPUT HOLDOFF", horizontal=True),
 
@@ -24,7 +24,7 @@ cams = [
     # break.
     Cam(600, -430, [(0.02, 0.04), (0.30,0.04)], 0, "SENDER EJECT", horizontal=True),
     # Cam 5: Regenerator 1
-    Cam(800, 100, [(0.24,0.05), (0.56,0.05)], 0, "UPPER REGEN CONTROL", horizontal=True),
+    Cam(800, 100, [(0.24,0.05), (0.56,0.05)], 0, "UPPER REGEN", horizontal=True),
 
     # Cam 6: Split to instruction register
     Cam(400,-120, [(0.18, 0.12)], 2, "TO INSTRUCTION REGISTER", horizontal=True, reverse_direction=True, bump_height=4),
@@ -41,24 +41,28 @@ cams = [
     # Cam 12: Fires main memory injector, injecting all 8 columns. If STO is on, this diverts to the subtractor reader. If not, it
     # will fall through the memory and be discarded.
     Cam(0, 300, [(0.62,0.02)], 1, "MAIN INJECTOR"),
-        # Cam 13: Divert to subtractor reader on STO.
-        # Also diverts the regenerator output on STO; we must separately discard that.
+
+    # Cam 13: Divert to subtractor reader on STO.
+    # Also diverts the regenerator output on STO; we must separately discard that.
     Cam(1000, 0, [(0.49,0.2)], 2, "STO TRIGGER", horizontal=True, reverse_direction=True, bump_height=3.5),
+
     # Cam 14: Divert to instruction pointer, on JRP (and JMP via the same lever).
     # Cam pattern *nearly* identical to #13, please adjust to see if it works
     Cam(1100, 0, [(0.5,0.2)], 2, "JRP TRIGGER", horizontal=True, reverse_direction=True),
 
-    # Cam 15: Secondary discard, of any data falling through the memory just after main inject
-    Cam(-500,-150, [(0.67,0.07)], 2, "DISCARD 2", reverse_direction=True, horizontal=True),
-        # Cam 16: Fires bottom regenerator (usually empty, unless STO is on)
-    Cam(-500,-300, [(0.87,0.02)], 0, "LOWER REGEN CONTROL", horizontal=True),
+    # Cam 15: Discard of any data falling through the memory just after main inject
+    Cam(-500,-150, [(0.67,0.07)], 2, "DISCARD", reverse_direction=True, horizontal=True),
 
-        # Cam 17: Reset PC on JMP
+    # Cam 16: Fires bottom regenerator (usually empty, unless STO is on)
+    Cam(-500,-300, [(0.87,0.02)], 0, "LOWER REGEN", horizontal=True),
+
+    # Cam 17: Reset PC on JMP
     Cam(1230, 0, [(0.5,0.1)], 2, "JMP TRIGGER", horizontal=True, reverse_direction=True),
 
     # Cam 18: Runs CMP.
     # Cam pattern is identical to #9.
     Cam(900,200, [(instruction_ready_point,0.05)], -1, "CMP TRIGGER", horizontal=True, reverse_direction=False),
+
     # Cam 19: Inc PC.
     Cam(-95,-450, [(0.85,0.05)], 0, "INC PC", horizontal=True, reverse_direction=False, bump_height=5, follower=False)
 
